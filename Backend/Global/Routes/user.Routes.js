24495da673b1d4ce59b923b98e398fb2079f2a9e
DevloperXMB <mehdi35yettou@gmail.com>
2024-10-20 14:29:42 +0100

    Delete Backend/Global directory

--- Backend/Global/Routes/user.Routes.js
+++
@@ -1,9 +0,0 @@
-// routes/userRoutes.js
-const express = require('express');
-const router = express.Router();
-const { registerUser } = require('../Controllers/userController');
-
-// POST /api/users/register - Register a new user
-router.post('/register', registerUser);
-
-module.exports = router;
